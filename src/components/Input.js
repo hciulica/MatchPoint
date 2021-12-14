e460fb1b7435fda00d4a900db89200a9b20bac60
Horia-Andrei CIULICA <horia46@yahoo.com>
2021-12-14 23:27:58 +0200

    Show/Hide password added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,34 +1,67 @@
 import {useLinkProps} from '@react-navigation/native';
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, TextInput, View, Text} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Feather from 'react-native-vector-icons/Feather';
 
-const Input = ({title, password, style, eyevisible}) => {
+const Input = ({title, passwordfield, iconshow}) => {
+  const [isSecureEntry, setIsSecureEntry] = useState(true);
+
+  console.log(isSecureEntry);
   return (
     <View>
       <Text style={styles.text}>{title}</Text>
-      <View>
-        {/* <Feather name = "eye" style = {styles.showeye}/> */}
+      <View style = { styles.backgroundStyle}>
           <TextInput
-            style={styles.input}
+            style={styles.inputStyle}
             placeholderTextColor={'darkgray'}
-            secureTextEntry={password}
+            secureTextEntry={passwordfield === false ? !isSecureEntry : isSecureEntry}
+            autoCapitalize="none"
+            autoCorrect={false}
           />
-      </View>
-      {/* <Feather name = "eye" style={styles.showeye}/> */}
-      {/* <Feather name = "eye-off"/> */}
-      
+
+          {iconshow ? 
+          <TouchableOpacity onPress = {()=>{
+            setIsSecureEntry((prev) => !prev)            
+          }}>
+          <View>
+            {
+              isSecureEntry ?
+                <Feather name = "eye-off" style = {styles.showeye}/>
+                :
+                <Feather name = "eye" style = {styles.showeye}/>
+            }
+            </View>
+          </TouchableOpacity>
+           : null}
+      </View>      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
+  backgroundStyle:{
+    marginTop: 15,
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    marginBottom: 10
+  },
   text: {
     marginHorizontal: 20,
     padding: 10,
     borderRadius: 3,
     color: '#A09C9C',
     fontSize: 13,
+  },
+  inputStyle: {
+    flex : 1,
+    fontSize: 18,
+    color: 'black',
+    marginHorizontal: 13
   },
   input: {
     marginHorizontal: 20,
@@ -39,11 +72,14 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     paddingLeft: 15,
     paddingRight: 15,
+    flexDirection: 'row',
+
   },
   showeye:{
     fontSize: 30,
-    marginLeft: 330,
-    borderRadius: 50
+        alignSelf:'center',
+        marginHorizontal: 15,
+        marginVertical: 10
   },
   
 });
